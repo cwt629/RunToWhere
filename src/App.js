90aa6728ef9497428b4f6c50e0634ef54ddb7281
wontae chang <cwt629@naver.com>
2023-03-09 15:10:29 +0900

    feat: get all data by fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import { getData } from "./utils/api.js"
+import { getAllData } from "./utils/api.js"
 
 export default function App($app) {
-    getData();
+    const init = async () => {
+        const data = await getAllData();
+        console.log("모든 데이터를 받아왔습니다.");
+        console.log(data);
+    }
+
+    init();
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,47 @@
 const BASE = "http://apis.data.go.kr/1741000/EmergencyAssemblyArea_Earthquake2/getArea1List";
 const ENC_KEY = "t%2BaPlQWgYTpT%2FIe4h2Ew%2FDfmCh3lHWaleDh311A5XZPa3DWZ3uiXuJ6se8aI4oO188kaRL6xO11aHuCw3VPtfw%3D%3D";
+const MAX_NUM_OF_ROWS = 1000;
 
-export async function getData() {
+export async function getAllData() {
+    // 첫 데이터 받아오기
+    const firstData = await getData(1);
+
+    // 전체 개수 받아오기
+    const totalCount = firstData.EarthquakeOutdoorsShelter[0].head[0].totalCount;
+
+    // 최대 페이지 수
+    const totalPage = Math.ceil(totalCount / MAX_NUM_OF_ROWS);
+
+    // 모든 데이터
+    let results = [...extractData(firstData)];
+
+    for (let page = 2; page <= totalPage; page++) {
+        const response = await getData(page);
+        results = [...results, ...extractData(response)];
+    }
+
+    return results;
+}
+
+export async function getData(page = 1) {
     const sendingData = {
         ServiceKey: ENC_KEY,
         type: "json",
-        pageNo: 1,
-        numOfRows: 1000
+        pageNo: page,
+        numOfRows: MAX_NUM_OF_ROWS
     };
 
     const url = `${BASE}?${getQuery(sendingData)}`; // 쿼리문과 함께 url 받아옴
 
     const response = await fetch(url);
     const result = await response.json();
-    console.log(result);
 
     return result;
+}
+
+// 받아온 데이터에서 각 지역 정보에 해당하는 부분만 추출해 반환하는 함수
+function extractData(data) {
+    return data.EarthquakeOutdoorsShelter[1].row;
 }
 
 // sendingData를 쿼리형식으로 변형하는 함수
